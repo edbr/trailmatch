d39b7576e56f185e01cda499920de7c6c887b41b
Eduardo Ribeiro <ribeiro.ebb@gmail.com>
2025-04-21 17:03:56 -0700

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script"
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -43,6 +44,7 @@ export default function RootLayout({
         </Script>
 
         {children}
+        <Analytics /> {/* ✅ Add this here */}
       </body>
     </html>
   );
