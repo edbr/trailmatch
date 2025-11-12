529a094210dde5d488b3dd5ffc56aba39ad3bbd2
Eduardo Ribeiro <ribeiro.ebb@gmail.com>
2025-11-12 11:55:18 -0800

    big one third time, fixing lint

--- src/components/ResultsSearchBar.tsx
+++ src/components/ResultsSearchBar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect, FormEvent, RefObject } from "react"
+import { useState, FormEvent, RefObject } from "react"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { useRouter } from "next/navigation"
